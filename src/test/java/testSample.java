0ea4fc9ad1b01882b1272189a2dcf9d840ba5249
elotfy <elotfy@sequel.ae>
2023-10-09 23:26:17 +0200

    Excel driver

---
+++ src/test/java/testSample.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class testSample {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		String Values ;
+
+		dataDriven d=new dataDriven();
+	ArrayList data=d.getData("Logout","TestCases","sample", "src/test/resources/demodata.xlsx");
+
+
+//	for (int i = 0; i< data.size();i++){
+//
+//		Values =  data.get(i).toString();
+//
+//		System.out.println(Values);
+//
+//	}
+
+		System.out.println(data.get(0));
+		System.out.println(data.get(1));
+		System.out.println(data.get(2));
+
+
+
+	}
+
+}
